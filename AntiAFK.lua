5a55b9ff82516804a092d52cd0558626fe5dd0f8
amWawa <128014481+amWawa@users.noreply.github.com>
2023-03-15 22:10:01 -0600

    Update AntiAFK.lua

--- AntiAFK.lua
+++ AntiAFK.lua
@@ -4,11 +4,11 @@ repeat task.wait() until game:GetService("Workspace").Players:FindFirstChild(gam
 local LocalPlayer = Players.LocalPlayer
 local StarterGui = game:GetService("StarterGui")
 
-LocalPlayer.Idled:Connect(function()
+--[[LocalPlayer.Idled:Connect(function()
     VirtualUser:Button2Down(Vector2.new(0, 0), Workspace.CurrentCamera.CFrame)
     task.wait(1)
     VirtualUser:Button2Up(Vector2.new(0, 0), Workspace.CurrentCamera.CFrame)
-end)
+end)]]
 
 StarterGui:SetCore("SendNotification", { Title = "Anti AFK", Text = "Injected", Duration = 5 })
 
